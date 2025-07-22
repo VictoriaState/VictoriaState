38577bd8ea4595a99a29e89b5bd51842b5f56b0d
AmericanLife <52460339+AbrahamMoody@users.noreply.github.com>
2025-07-22 13:36:26 +0200

    VORP neu strukturiert
    VORP neu strukturiert

--- txdata/VictoriaState/resources/[VORP]/vorp_inventory/html/js/config.js
+++
@@ -1,9 +0,0 @@
-var Config = new Object();
-Config.closeKeys = [113, 27];
-Config.UseGoldItem = false;
-Config.AddGoldItem = false;
-Config.AddDollarItem = false;
-Config.AddAmmoItem = false;
-Config.DoubleClickToUse = true;
-Config.WeightMeasure = "kg";
-Config.MaxItemTransferAmount = 200;
--- txdata/VictoriaState/resources/[VORP]/vorp_wildhorse/README.md
+++
@@ -1,8 +0,0 @@
-# vorp_sell wild horses
-
-*  ensure the script in resources.cfg
-*  add webhook for cheating logs 
-
-# credits
-
-* @CallMeNc 
